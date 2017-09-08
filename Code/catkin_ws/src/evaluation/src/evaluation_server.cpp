a6f3802bd2a55ef9d62aafae01b0106222416961
Fabian <2schmid@informatik.uni-hamburg.de>
2017-09-08 16:54:35 +0200

    publishing ground truth transforms

--- Code/catkin_ws/src/evaluation/src/evaluation_server.cpp
+++ Code/catkin_ws/src/evaluation/src/evaluation_server.cpp
@@ -9,6 +9,7 @@
 #include <octomap_msgs/MergeCandidates.h>
 #include <candidate_locator/ArrayPointClouds.h>
 
+#include <tf/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 #include <sensor_msgs/image_encodings.h>
@@ -25,94 +26,123 @@
 
 #include <boost/foreach.hpp>
 
-bool evaluate(evaluation::Evaluate::Request  &req,
-         evaluation::Evaluate::Response &res)
+namespace evaluation
 {
-  std::vector<std::string> objects;
-  objects.push_back("ball");
-  objects.push_back("banana");
-  objects.push_back("bleach_cleanser");
-  objects.push_back("bowl");
-  objects.push_back("cracker_box");
-  objects.push_back("hammer");
-  objects.push_back("mug");
-  objects.push_back("pitcher_base");
-  objects.push_back("potted_meat_can");
-  objects.push_back("power_drill");
-  objects.push_back("sugar_box");
-  objects.push_back("wood_block");
+  class EvaluationServer
+	  {
+		  private:
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PCDReader reader;
-  
-  std::string objects_package_path(ros::package::getPath("objects"));
-  std::string fileName;
-  candidate_locator::ArrayPointClouds array_pc_msg; 
-  
-  ros::NodeHandle nh;  
-  ros::ServiceClient gms_client = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
-  gazebo_msgs::GetModelState getmodelstate;
-  //ros::ServiceClient gms_client = nh.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
-  //gazebo_msgs::GetLinkState getmodelstate;
-  gms_client.waitForExistence();  
-  
-  geometry_msgs::TransformStamped transform;
-  transform.header.frame_id = "/map";
-  transform.child_frame_id = "/map";
-  
-  BOOST_FOREACH(std::string object, objects){  
-    fileName = objects_package_path + "/data/"+object+"/"+object+"_downsampled.pcd";    
-    reader.read (fileName, *cloud);  
-    
-    sensor_msgs::PointCloud2 pc_msg;    
-    pcl::toROSMsg<pcl::PointXYZ>(*cloud,pc_msg); 
-    
-    getmodelstate.request.model_name = object;
-    //getmodelstate.request.link_name = object+"_link";
-    gms_client.call(getmodelstate);
-    
-    std::cout << getmodelstate.response.pose << std::endl;
-    //std::cout << getmodelstate.response.link_state.pose << std::endl;
-    
-    transform.transform.translation.x = getmodelstate.response.pose.position.x;
-    transform.transform.translation.y = getmodelstate.response.pose.position.y;
-    transform.transform.translation.z = getmodelstate.response.pose.position.z;
-    transform.transform.rotation = getmodelstate.response.pose.orientation;
-    
-    //tf2::doTransform (cloud_in, cloud_out, transform);
-    tf2::doTransform(pc_msg, pc_msg, transform);
-    
-    array_pc_msg.data.push_back(pc_msg);
-  }
-  
-  // ros::ServiceClient comparison_client = nh.serviceClient<evaluation::CompareGroundTruthsToProposals>("compare_ground_truths_to_proposals"); 
-  ros::ServiceClient comparison_client = nh.serviceClient<octomap_msgs::MergeCandidates>("octomap_server/merge_candidates"); 
-  comparison_client.waitForExistence();
-  
-  // evaluation::CompareGroundTruthsToProposals comparison_srv;
-  octomap_msgs::MergeCandidates comparison_srv;
-  comparison_srv.request.candidates = array_pc_msg;
-  
-  ROS_INFO("Requesting to compare ground truths to proposals");
-  if (comparison_client.call(comparison_srv))
-  {
-    ROS_INFO("Received statistics of comparison");
-  }
-  else
-  {
-    ROS_ERROR("Failed to compare ground truths to proposals");
-  }
-  
-  return true;
+		  ros::NodeHandle nh_;
+		  tf::TransformListener listener_;
+
+		  public:
+
+      bool evaluate(evaluation::Evaluate::Request  &req,
+               evaluation::Evaluate::Response &res)
+      {
+        std::vector<std::string> objects;
+        objects.push_back("ball");
+        objects.push_back("banana");
+        objects.push_back("bleach_cleanser");
+        objects.push_back("bowl");
+        objects.push_back("cracker_box");
+        objects.push_back("hammer");
+        objects.push_back("mug");
+        objects.push_back("pitcher_base");
+        objects.push_back("potted_meat_can");
+        objects.push_back("power_drill");
+        objects.push_back("sugar_box");
+        objects.push_back("wood_block");
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PCDReader reader;
+        
+        std::string objects_package_path(ros::package::getPath("objects"));
+        std::string fileName;
+        candidate_locator::ArrayPointClouds array_pc_msg; 
+        
+        /*
+        ros::ServiceClient gms_client = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+        gazebo_msgs::GetModelState getmodelstate;
+        //ros::ServiceClient gms_client = nh.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
+        //gazebo_msgs::GetLinkState getmodelstate;
+        gms_client.waitForExistence();  
+        
+        
+        geometry_msgs::TransformStamped transform;
+        transform.header.frame_id = "/map";
+        transform.child_frame_id = "/map";
+        */
+        
+        tf::StampedTransform tf_transform;
+        
+        geometry_msgs::TransformStamped transform;
+        transform.header.frame_id = "/map";  
+        
+        BOOST_FOREACH(std::string object, objects){  
+          fileName = objects_package_path + "/data/"+object+"/"+object+"_downsampled.pcd";    
+          reader.read (fileName, *cloud);  
+          
+          sensor_msgs::PointCloud2 pc_msg;    
+          pcl::toROSMsg<pcl::PointXYZ>(*cloud,pc_msg); 
+          std::string frame = "/" + object;
+          try{
+            listener_.lookupTransform(frame, "/map",  
+                                     ros::Time(0), tf_transform);
+          }
+          catch (tf::TransformException ex){
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
+          }
+          
+          std::cout << tf_transform.getOrigin() << std::endl;
+         
+          transform.child_frame_id = frame;
+          transform.transform.translation.x = tf_transform.getOrigin().getX();
+          transform.transform.translation.y = tf_transform.getOrigin().getY();
+          transform.transform.translation.z = tf_transform.getOrigin().getZ();
+          transform.transform.rotation.x = tf_transform.getRotation().getAxis().getX();
+          transform.transform.rotation.y = tf_transform.getRotation().getAxis().getY();
+          transform.transform.rotation.z = tf_transform.getRotation().getAxis().getZ();
+          transform.transform.rotation.w = tf_transform.getRotation().getW();
+         
+          //tf2::doTransform (cloud_in, cloud_out, transform);
+          tf2::doTransform(pc_msg, pc_msg, transform);
+          
+          array_pc_msg.data.push_back(pc_msg);
+        }
+        
+        // ros::ServiceClient comparison_client = nh.serviceClient<evaluation::CompareGroundTruthsToProposals>("compare_ground_truths_to_proposals"); 
+        ros::ServiceClient comparison_client = nh_.serviceClient<octomap_msgs::MergeCandidates>("octomap_server/merge_candidates"); 
+        comparison_client.waitForExistence();
+        
+        // evaluation::CompareGroundTruthsToProposals comparison_srv;
+        octomap_msgs::MergeCandidates comparison_srv;
+        comparison_srv.request.candidates = array_pc_msg;
+        
+        ROS_INFO("Requesting to compare ground truths to proposals");
+        if (comparison_client.call(comparison_srv))
+        {
+          ROS_INFO("Received statistics of comparison");
+        }
+        else
+        {
+          ROS_ERROR("Failed to compare ground truths to proposals");
+        }
+        
+        return true;
+      }
+  };
 }
-
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "evaluation_server");
   ros::NodeHandle nh;
   
-  ros::ServiceServer service = nh.advertiseService("evaluate", evaluate);
+  evaluation::EvaluationServer server;
+  
+  ros::ServiceServer service = nh.advertiseService("evaluate", &evaluation::EvaluationServer::evaluate, &server);
   
   ROS_INFO("Ready to evaluate");
   ros::spin();
---
+++ Code/catkin_ws/src/evaluation/src/transform_publisher.cpp
@@ -0,0 +1,58 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+#include <gazebo_msgs/GetModelState.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <string>
+#include <boost/foreach.hpp>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "ground_truth_tf_broadcaster");
+  ros::NodeHandle nh;
+  
+  std::vector<std::string> objects;
+  objects.push_back("ball");
+  objects.push_back("banana");
+  objects.push_back("bleach_cleanser");
+  objects.push_back("bowl");
+  objects.push_back("cracker_box");
+  objects.push_back("hammer");
+  objects.push_back("mug");
+  objects.push_back("pitcher_base");
+  objects.push_back("potted_meat_can");
+  objects.push_back("power_drill");
+  objects.push_back("sugar_box");
+  objects.push_back("wood_block");
+
+  ros::ServiceClient gms_client = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+  gazebo_msgs::GetModelState getmodelstate;
+  gms_client.waitForExistence();  
+ 
+  std::vector<tf::Transform> transforms;
+  
+  int idx = 0;
+  
+  BOOST_FOREACH(std::string object, objects){  
+    getmodelstate.request.model_name = object;
+    gms_client.call(getmodelstate);
+    tf::Transform transform;
+    transform.setOrigin( tf::Vector3(getmodelstate.response.pose.position.x, getmodelstate.response.pose.position.y, getmodelstate.response.pose.position.z) );
+    transform.setRotation( tf::Quaternion(getmodelstate.response.pose.orientation.x, getmodelstate.response.pose.orientation.y, getmodelstate.response.pose.orientation.z, getmodelstate.response.pose.orientation.w) );
+    transforms.push_back(transform);
+    ++idx;
+  }
+
+  tf::TransformBroadcaster br; 
+  ros::Rate rate(1.0);
+  while (nh.ok()){
+    int idx = 0;
+    BOOST_FOREACH(std::string object, objects){  
+      br.sendTransform(tf::StampedTransform(transforms[idx], ros::Time::now(), "/map", object));
+      ++idx;
+    }
+    rate.sleep();
+  }
+  return 0;
+};
+
